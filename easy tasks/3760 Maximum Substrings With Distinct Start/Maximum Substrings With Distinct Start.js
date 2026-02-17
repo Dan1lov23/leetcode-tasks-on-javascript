0a15b95ebfa0e4d219c40ea11ec7775772f1c5b4
kitik <svy1dan@gmail.com>
2026-02-17 16:48:10 +0300

    add new task

---
+++ easy tasks/3760 Maximum Substrings With Distinct Start/Maximum Substrings With Distinct Start.js
@@ -0,0 +1,65 @@
+// Maximum Substrings With Distinct Start
+
+// You are given a string s consisting of lowercase English letters.
+//
+// Return an integer denoting the maximum number of substrings you can split s into such that each substring starts with a distinct character (i.e., no two substrings start with the same character).
+//
+//
+//
+// Example 1:
+//
+// Input: s = "abab"
+//
+// Output: 2
+//
+// Explanation:
+//
+// Split "abab" into "a" and "bab".
+// Each substring starts with a distinct character i.e 'a' and 'b'. Thus, the answer is 2.
+// Example 2:
+//
+// Input: s = "abcd"
+//
+// Output: 4
+//
+// Explanation:
+//
+// Split "abcd" into "a", "b", "c", and "d".
+// Each substring starts with a distinct character. Thus, the answer is 4.
+// Example 3:
+//
+// Input: s = "aaaa"
+//
+// Output: 1
+//
+// Explanation:
+//
+// All characters in "aaaa" are 'a'.
+// Only one substring can start with 'a'. Thus, the answer is 1.
+//
+//
+// Constraints:
+//
+// 1 <= s.length <= 105
+// s consists of lowercase English letters.
+
+function MaximumSubstringsWithDistinctStart(s) {
+
+    let counter = 0;
+
+    let includeElement = [];
+
+    for (let a = 0; a <= s.length; a++) {
+        let actualElement = s[0];
+        if (actualElement !== s[a] && !includeElement.includes(s[a])) {
+            counter++;
+            actualElement = s[a];
+            includeElement.push(s[a]);
+        }
+    }
+
+    return counter;
+
+}
+
+console.log(MaximumSubstringsWithDistinctStart("aaaa"))
